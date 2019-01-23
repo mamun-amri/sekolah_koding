0ed9ea1bca908245d64b30fd2682d2fbaea70a02
tafrij <mamunamri97@gmail.com>
2019-01-23 21:23:42 +0700

    git push * origin master

--- membuat_blog/functions/blog.php
+++ membuat_blog/functions/blog.php
@@ -8,4 +8,19 @@
     return $result;
   }
 
+  function tambah($judul,$isi,$tag)
+  {
+    global $link;
+
+    $query = "INSERT INTO blog (judul,isi,tag) VALUES ('$judul','$isi','$tag')";
+    return run($query);
+  }
+
+  function run($query)
+  {
+    global $link;
+
+    if($result=mysqli_query($link,$query)) return true;
+    else return false;
+  }
  ?>
--- membuat_blog/view/header.php
+++ membuat_blog/view/header.php
@@ -1,3 +1,8 @@
 <title>mbanten</title>
 <link rel="stylesheet" href="view/style.css">
 <h1>Selamat Datang di MBanten</h1>
+
+<div id="menu">
+  <a href="index.php">Home</a>
+  <a href="tambah.php">Tambah</a>
+</div>
