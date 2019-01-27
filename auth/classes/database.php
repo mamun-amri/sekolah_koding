f5769beaf2441576c5696530e0ef0b6b92798af6
tafrij <mamunamri97@gmail.com>
2019-01-27 20:50:00 +0700

    git push * origin master

---
+++ auth/classes/database.php
@@ -0,0 +1,32 @@
+<?php
+
+  class database{
+
+    public static $INSTANCE = null;
+    private $mysqli ,
+            $HOST   = 'localhost',
+            $USER   = 'root',
+            $PASS   = '',
+            $DBNAME = 'sekolah_koding';
+
+    public function __construct() {
+      $this->mysqli = new mysqli ( $this->HOST,$this->USER,$this->PASS,$this->DBNAME );
+      if( mysqli_connect_error() ){
+        die('gagal koneksi');
+      }
+    }
+
+    /*
+    singleton pattern
+    menguji koneksi agar tidak double
+    */
+    public static function getInstance(){
+      if( !isset( self::$INSTANCE ) ){
+        self::$INSTANCE = new database();
+      }
+      return self::$INSTANCE;
+    }
+
+  }
+
+ ?>
---
+++ auth/core/init.php
@@ -0,0 +1,9 @@
+<?php
+
+  session_start();
+  // load kelas yang ada di folder classes
+  spl_autoload_register( function($class){
+    require_once 'classes/' .$class. '.php';
+  });
+
+ ?>
